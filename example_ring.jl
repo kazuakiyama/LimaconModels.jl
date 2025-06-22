4b22fb97df2f54fbc05bc0d8ca9d0808a4e6eafa
Kazunori Akiyama <kakiyama@mit.edu>
2025-06-22 12:00:54 -0400

    update comments

--- example_ring.jl
+++ example_ring.jl
@@ -9,9 +9,6 @@ using CairoMakie
 # limacon shape
 λ1 = μas2rad(20)
 λ2 = 0.5
-# brightness profile
-α = [0.2]
-β = [0.0]
 # rotation
 PA = π / 2
 # blurring kernel FWHM
@@ -22,17 +19,16 @@ fov = μas2rad(100)
 nx = 1000
 
 #
-# Create a m-Limacon model
+# Create a uniform Limacon model
 #
 
 # create the image grid
 grid = imagepixels(fov, fov, nx, nx)
 
-# create a m-limacon model
-#.  resize Limacon, rotate by -90 deg, and then convolve
+# create a uniform limacon model
 mlmodel = DblGaussianLimacon(λ1, λ2, σinner, σouter, PA, 0, 0)
 
-# map out the m-limacon model to the image grid
+# map out the limacon model to the image grid
 iml = intensitymap(mlmodel, grid)
 
 # plot
